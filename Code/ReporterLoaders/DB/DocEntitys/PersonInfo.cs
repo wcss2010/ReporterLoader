47a3e2d7bf402215a6cb2c063c5492ac993f57e0
wcss2020@gmail.com <wcss2020@gmail.com>
2019-01-07 10:04:51 +0800

    ...

---
+++ Code/ReporterLoaders/DB/DocEntitys/PersonInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReporterLoaders.DB.DocEntitys
+{
+    public class PersonInfo
+    {
+        /// <summary>
+        /// 数据字典
+        /// </summary>
+        public Dictionary<string, string> BaseInfoDict = new Dictionary<string, string>();
+    }
+}
--- Code/ReporterLoaders/MainForm.cs
+++ Code/ReporterLoaders/MainForm.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Windows.Forms;
 using Aspose.Words;
 using Aspose.Words.Tables;
+using ReporterLoaders.DB.DocEntitys;
 
 namespace ReporterLoaders
 {
@@ -21,15 +22,39 @@ namespace ReporterLoaders
         {
             Document doc = new Document(@"C:\Users\wcss\Desktop\ReporterLoader\Docs\XX2.doc");
             Table t = (Table)doc.GetChild(NodeType.Table, 0, true);
+            PersonInfo bi = new PersonInfo();
+            string sKey,sValue;
+            sKey = string.Empty;
+            sValue = string.Empty;
             foreach (Row r in t.Rows)
             {
                 foreach (Cell c in r.Cells)
                 {
-                    System.Console.WriteLine(c.GetText());
+                    string s = c.GetText().Trim().Replace("\a", string.Empty).Replace("\0", string.Empty).Replace("\n", string.Empty).Replace("\r", string.Empty);
+                    if (string.IsNullOrEmpty(sKey))
+                    {
+                        if (string.IsNullOrEmpty(s))
+                        {
+                            continue;
+                        }
+
+                        sKey = s;
+                    }
+                    else if (string.IsNullOrEmpty(sValue))
+                    {
+                        sValue = s;
+
+                        bi.BaseInfoDict.Add(sKey, sValue);
+                        sKey = string.Empty;
+                        sValue = string.Empty;
+                    }
                 }
             }
 
-            
+            foreach (KeyValuePair<string, string> kvp in bi.BaseInfoDict)
+            {
+                System.Console.WriteLine(kvp.Key + "," + kvp.Value);
+            }
         }
     }
 }
